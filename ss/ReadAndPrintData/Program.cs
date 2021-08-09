a9988826c507d7125ca1cd7885c4032234579ba1
Tony Qu <>
2021-08-09 20:01:17 +0800

    add ReadAndPrintData

---
+++ ss/ReadAndPrintData/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using NPOI.SS.UserModel;
+
+namespace ReadAndPrintData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var workbook = WorkbookFactory.Create("data.xlsx");
+            ISheet sheet = workbook.GetSheetAt(0);
+            foreach (IRow row in sheet)
+            {
+                for (var i = 0; i < row.LastCellNum; i++)
+                {
+                    var cell = row.GetCell(i);
+                    if (cell != null)
+                    {
+                        Console.Write(cell.ToString());
+                        Console.Write("\t");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.Read();
+        }
+    }
+}
