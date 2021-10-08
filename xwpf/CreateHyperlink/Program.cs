9beec5afcf1b94e376d548b00a95505d5571130f
Tony Qu <tonyqus@gmail.com>
2021-10-09 00:03:07 +0800

    add CreateHyperlink example for XWPF

---
+++ xwpf/CreateHyperlink/Program.cs
@@ -0,0 +1,41 @@
+﻿using NPOI.OpenXmlFormats.Wordprocessing;
+using NPOI.XWPF.UserModel;
+using System;
+using System.IO;
+
+namespace CreateHyperlink
+{
+    class Program
+    {
+        static XWPFHyperlinkRun CreateHyperlinkRun(XWPFParagraph paragraph, String uri)
+        {
+            String rId = paragraph.Document.GetPackagePart().AddExternalRelationship(
+              uri,
+              XWPFRelation.HYPERLINK.Relation
+             ).Id;
+
+            return paragraph.CreateHyperlinkRun(rId);
+        }
+        static void Main(string[] args)
+        {
+
+            XWPFDocument doc = new XWPFDocument();
+
+            XWPFParagraph paragraph = doc.CreateParagraph();
+            XWPFRun run = paragraph.CreateRun();
+            run.SetText("This is a text paragraph having ");
+
+            XWPFHyperlinkRun hyperlinkrun = CreateHyperlinkRun(paragraph, "https://www.google.com");
+            hyperlinkrun.SetText("a link to Google");
+            hyperlinkrun.SetColor("0000FF");
+            hyperlinkrun.SetUnderline(UnderlinePatterns.Single);
+
+            run = paragraph.CreateRun();
+            run.SetText(" in it.");
+            using (FileStream out1 = new FileStream("hyperlink.docx", FileMode.Create))
+            {
+                doc.Write(out1);
+            }
+        }
+    }
+}
