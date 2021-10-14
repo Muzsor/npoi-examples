b931e4b89015662575da325536b1d3ea7b772b32
Tony Qu <tonyqus@gmail.com>
2021-10-14 12:37:42 +0800

    update MergeCell sample
    how to use RegionUtil

--- ss/MeringCells/Program.cs
+++ ss/MeringCells/Program.cs
@@ -1,4 +1,5 @@
-﻿using NPOI.SS.UserModel;
+﻿using NPOI.HSSF.Util;
+using NPOI.SS.UserModel;
 using NPOI.SS.Util;
 using NPOI.XSSF.UserModel;
 using System.IO;
@@ -10,17 +11,31 @@ namespace NPOI.Examples.XSSF.MeringCellsInXlsx
         static void Main(string[] args)
         {
             IWorkbook workbook = new XSSFWorkbook();
-            ISheet sheet = workbook.CreateSheet("PictureSheet");
+            ISheet sheet = workbook.CreateSheet();
 
-            IRow row = sheet.CreateRow(1);
-            ICell cell = row.CreateCell(1);
-            cell.SetCellValue(new XSSFRichTextString("This is a test of merging"));
+            var cell = sheet.CreateRow(1).CreateCell(1);
+            cell.SetCellValue(new XSSFRichTextString("This test"));
+
+            var style1=workbook.CreateCellStyle();
+            style1.Alignment = HorizontalAlignment.Center;
+            style1.FillForegroundColor = NPOI.HSSF.Util.HSSFColor.Yellow.Index2;
+            style1.FillPattern = FillPattern.SolidForeground;
+
+            cell.CellStyle = style1;
 
             sheet.AddMergedRegion(new CellRangeAddress(1, 1, 1, 2));
 
-            FileStream sw = File.OpenWrite("test.xlsx");
-            workbook.Write(sw);
-            sw.Close();
+            var cra=new CellRangeAddress(2, 3, 4, 5);
+            RegionUtil.SetBorderTop((int)BorderStyle.DashDot, cra, sheet);
+            RegionUtil.SetBorderLeft((int)BorderStyle.DashDot, cra, sheet);
+            RegionUtil.SetBorderRight((int)BorderStyle.DashDot, cra, sheet);
+            RegionUtil.SetBorderBottom((int)BorderStyle.DashDot, cra, sheet);
+            sheet.AddMergedRegion(cra);
+
+            using (FileStream sw = File.Create("test.xlsx"))
+            {
+                workbook.Write(sw);
+            }
         }
     }
 }
