e8b0ce36da42362d4c0c5356858924dc02736f85
Bryan Kho <bryan.kho@bluace.nl>
2022-09-13 14:11:36 +0200

    added mapping example

---
+++ xwpf/MapObjectToTable/Program.cs
@@ -0,0 +1,31 @@
+﻿using NPOI.XWPF.UserModel;
+using System.IO;
+
+namespace MapObjectToTable
+{
+    class Program
+    {
+        static void Main()
+        {
+            XWPFDocument document = new XWPFDocument();
+            XWPFTableWrapper<ExampleClass> newWrapper = new XWPFTableWrapper<ExampleClass>(document);
+
+            ExampleClass exampleData = new ExampleClass()
+            {
+                Enum = ExampleEnum.Red,
+                Name = "This is a test",
+                IgnoredMember = "This member will be ignored",
+                Address = new ExampleChildClass()
+                {
+                    Address = "Burgemeester Schönfeldplein",
+                    City = "Winschoten",
+                    CountryCode = "NL"
+                }
+            };
+            newWrapper.AddRow(exampleData);
+
+            using FileStream fs = new FileStream("test.docx", FileMode.Create);
+            document.Write(fs);
+        }
+    }
+}
