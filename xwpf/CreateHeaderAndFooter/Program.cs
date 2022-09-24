5844a7ce38fb62cb8c23b5e41fc25e608bf2f749
Tony Qu <tonyqus@gmail.com>
2022-09-24 12:14:47 +0800

    using NPOI 2.6.0

--- xwpf/CreateHeaderAndFooter/Program.cs
+++ xwpf/CreateHeaderAndFooter/Program.cs
@@ -10,52 +10,44 @@ namespace CreateHeaderAndFooter
     {
         static void Main(string[] args)
         {
-            XWPFDocument document = new XWPFDocument();
-
-            // create header-footer
-            XWPFHeaderFooterPolicy headerFooterPolicy = document.GetHeaderFooterPolicy();
-            if (headerFooterPolicy == null) headerFooterPolicy = document.CreateHeaderFooterPolicy();
-
-            // create header start
-            XWPFHeader header = headerFooterPolicy.CreateHeader(XWPFHeaderFooterPolicy.DEFAULT);
-
-            XWPFParagraph paragraph = header.CreateParagraph();
-            paragraph.Alignment= (ParagraphAlignment.CENTER);
-
-            XWPFRun run = paragraph.CreateRun();
-            run.SetText("Header");
-
-            // create footer start
-            XWPFFooter footer = headerFooterPolicy.CreateFooter(XWPFHeaderFooterPolicy.DEFAULT);
-
-            paragraph = footer.CreateParagraph();
-            paragraph.Alignment=(ParagraphAlignment.CENTER);
-
-            run = paragraph.CreateRun();
-            run.SetText("Footer");
-
-            CT_SectPr sectPr = document.Document.body.sectPr;
-            if (sectPr == null)
-                document.Document.body.AddNewSectPr();
-                sectPr = document.Document.body.sectPr;
-            CT_PageMar pageMar = sectPr.pgMar;
-            if (pageMar == null)
+            using (XWPFDocument document = new XWPFDocument())
             {
-                sectPr.pgMar = new CT_PageMar();
-                pageMar = sectPr.pgMar;
-            }
-            pageMar.left=720; //720 TWentieths of an Inch Point (Twips) = 720/20 = 36 pt = 36/72 = 0.5"
-            pageMar.right=720;
-            pageMar.top="1440"; //1440 Twips = 1440/20 = 72 pt = 72/72 = 1"
-            pageMar.bottom="1440";
 
-            pageMar.header=908; //45.4 pt * 20 = 908 = 45.4 pt header from top
-            pageMar.footer=568; //28.4 pt * 20 = 568 = 28.4 pt footer from bottom
+                // create header-footer
+                XWPFHeaderFooterPolicy headerFooterPolicy = document.GetHeaderFooterPolicy();
+                if (headerFooterPolicy == null) headerFooterPolicy = document.CreateHeaderFooterPolicy();
 
-            using (var fs = File.Create("CreateWordHeaderFooterTopBottom.docx"))
-            {
-                document.Write(fs);
-                document.Close();
+                // create header start
+                XWPFHeader header = headerFooterPolicy.CreateHeader(XWPFHeaderFooterPolicy.DEFAULT);
+
+                XWPFParagraph paragraph = header.CreateParagraph();
+                paragraph.Alignment = (ParagraphAlignment.CENTER);
+
+                XWPFRun run = paragraph.CreateRun();
+                run.SetText("Header");
+
+                // create footer start
+                XWPFFooter footer = headerFooterPolicy.CreateFooter(XWPFHeaderFooterPolicy.DEFAULT);
+
+                paragraph = footer.CreateParagraph();
+                paragraph.Alignment = (ParagraphAlignment.CENTER);
+
+                run = paragraph.CreateRun();
+                run.SetText("Footer");
+
+                CT_SectPr sectPr = document.Document.body.sectPr;
+                CT_PageMar pageMar = sectPr.AddPageMar();
+                pageMar.left = 720; //720 TWentieths of an Inch Point (Twips) = 720/20 = 36 pt = 36/72 = 0.5"
+                pageMar.right = 720;
+                pageMar.top = 1440; //1440 Twips = 1440/20 = 72 pt = 72/72 = 1"
+                pageMar.bottom = 1440;
+                pageMar.header = 908; //45.4 pt * 20 = 908 = 45.4 pt header from top
+                pageMar.footer = 568; //28.4 pt * 20 = 568 = 28.4 pt footer from bottom
+
+                using (var fs = File.Create("CreateWordHeaderFooterTopBottom.docx"))
+                {
+                    document.Write(fs);
+                }
             }
             
         }
