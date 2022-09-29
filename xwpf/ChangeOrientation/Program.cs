228c2f1dff49f7af291aa2d8cb8e650e4765859f
Tony Qu <tonyqus@gmail.com>
2022-09-30 05:20:31 +0800

    Add example: ChangeOrientation

---
+++ xwpf/ChangeOrientation/Program.cs
@@ -0,0 +1,24 @@
+﻿using NPOI.XWPF.UserModel;
+using System;
+using System.IO;
+
+namespace ChangeOrientation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XWPFDocument doc = new XWPFDocument();
+
+            var run = doc.CreateParagraph().CreateRun();
+            run.SetText("Hello World!");
+
+            doc.ChangeOrientation(NPOI.OpenXmlFormats.Wordprocessing.ST_PageOrientation.landscape);
+            using (FileStream fs = new FileStream("test.docx", FileMode.Create))
+            {
+                doc.Write(fs);
+                doc.Close();
+            }
+        }
+    }
+}
