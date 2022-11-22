ff4785e6515e5fb3f39639314122448bb83817e3
Tony Qu <tonyqus@gmail.com>
2022-11-22 17:22:36 +0800

    Add example: FillDateOnlyValue (This is a .net 6 sample)

---
+++ xssf/FillDateOnlyValue/Program.cs
@@ -0,0 +1,32 @@
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+using (IWorkbook workbook = new XSSFWorkbook())
+{
+    ISheet sheet = workbook.CreateSheet("Sheet1");
+    //increase the width of Column A
+    sheet.SetColumnWidth(0, 5000);
+    //create the format instance
+    IDataFormat format = workbook.CreateDataFormat();
+
+    
+    //Chinese date string
+    ICell cell7 = sheet.CreateRow(6).CreateCell(0);
+    SetValueAndFormat(workbook, cell7, new DateOnly(2004, 5, 6), format.GetFormat("yyyy年m月d日"));
+
+    using (FileStream sw = File.Create("test.xlsx"))
+    {
+        workbook.Write(sw, false);
+    }
+}
+
+static void SetValueAndFormat(IWorkbook workbook, ICell cell, DateOnly value, short formatId)
+{
+    //set value for the cell
+    cell.SetCellValue(value);
+
+    ICellStyle cellStyle = workbook.CreateCellStyle();
+    cellStyle.DataFormat = formatId;
+    cell.CellStyle = cellStyle;
+}
