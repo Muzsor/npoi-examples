838edcafe356a8487a49099133811d88dcfcdaca
Tony Qu <tonyqus@gmail.com>
2023-04-06 08:01:46 +0800

    add example: ExtractTextFromXlsx

---
+++ xssf/ExtractTextFromXlsx/Program.cs
@@ -0,0 +1,17 @@
+﻿using NPOI.XSSF.Extractor;
+using NPOI.XSSF.UserModel;
+using System.IO;
+
+
+using (var stream = File.OpenRead("text.xlsx"))
+using (var xssWorkbook = new XSSFWorkbook(stream))
+{
+    var excelExtractor = new XSSFExcelExtractor(xssWorkbook)
+    {
+        IncludeCellComments = false,
+        IncludeHeaderFooter = false
+    };
+    var stringCellValue = excelExtractor.Text;
+    Console.Write(stringCellValue);
+    Console.ReadLine();
+}
