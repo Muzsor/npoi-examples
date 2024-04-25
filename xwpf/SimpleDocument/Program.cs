fce49f7565c88ef923d84e3f64c787a2abdeb2f7
Tony Qu <tonyqus@gmail.com>
2024-04-26 07:34:31 +0800

    fix #25
    Use XWPFRun.AppendText instead

--- xwpf/SimpleDocument/Program.cs
+++ xwpf/SimpleDocument/Program.cs
@@ -71,7 +71,7 @@ namespace SimpleDocument
                         + "Or to take arms against a sea of troubles, "
                         + "And by opposing end them? To die: to sleep; ");
                 r4.AddBreak(BreakType.PAGE);
-                r4.SetText("No more; and by a sleep to say we end "
+                r4.AppendText("No more; and by a sleep to say we end "
                         + "The heart-ache and the thousand natural shocks "
                         + "That flesh is heir to, 'tis a consummation "
                         + "Devoutly to be wish'd. To die, to sleep; "
@@ -84,15 +84,15 @@ namespace SimpleDocument
                 r5.TextPosition = -10;
                 r5.SetText("For in that sleep of death what dreams may come");
                 r5.AddCarriageReturn();
-                r5.SetText("When we have shuffled off this mortal coil,"
+                r5.AppendText("When we have shuffled off this mortal coil,"
                         + "Must give us pause: there's the respect"
                         + "That makes calamity of so long life;");
                 r5.AddBreak();
-                r5.SetText("For who would bear the whips and scorns of time,"
+                r5.AppendText("For who would bear the whips and scorns of time,"
                         + "The oppressor's wrong, the proud man's contumely,");
 
                 r5.AddBreak(BreakClear.ALL);
-                r5.SetText("The pangs of despised love, the law's delay,"
+                r5.AppendText("The pangs of despised love, the law's delay,"
                         + "The insolence of office and the spurns" + ".......");
 
                 using (var fs = new FileStream("simple.docx", FileMode.Create))
